f135bf66cde3fa85eea476b769641752b35c87d4
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-09-03 23:40:07 +0100

    Create new BackstagePassItem class.

---
+++ csharp/BackstagePassItem.cs
@@ -0,0 +1,26 @@
+﻿namespace csharp
+{
+    public class BackstagePassItem
+    {
+        private Item _item;
+        public int Quality => _item.Quality;
+
+        public BackstagePassItem(int sell_in, int quality)
+        {
+            _item = new Item
+            {
+                Name = ItemNames.BackstagePasses, 
+                SellIn = sell_in, 
+                Quality = quality
+            };
+        }
+
+        public void Adjust_quality_if_sell_by_date_has_passed()
+        {
+            if (_item.SellIn < 0)
+            {
+                _item.Quality = 0;
+            }
+        }
+    }
+}
