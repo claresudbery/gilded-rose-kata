d5a23106eea1e505379f43ae988d3a4f2418220d
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-11-15 23:55:31 +0000

    Add AAA markers to tests

--- csharp/GildedRoseLegendaryItemTests.cs
+++ csharp/GildedRoseLegendaryItemTests.cs
@@ -12,20 +12,30 @@ namespace csharp
         [Test]
         public void After_daily_update_SellIn_value_for_sulfuras_remains_the_same()
         {
+            // Arrange
             int initial_sellin_value = _test_sulfuras_01.SellIn;
             IList<IItem> self_managing_items = new List<IItem> { _test_sulfuras_01 };
             GildedRose app = new GildedRose(self_managing_items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(initial_sellin_value, self_managing_items[0].SellIn);
         }
 
         [Test]
         public void After_daily_update_Quality_value_for_sulfuras_remains_the_same()
         {
+            // Arrange
             int initial_quality_value = _test_sulfuras_01.Quality;
             IList<IItem> self_managing_items = new List<IItem> { _test_sulfuras_01 };
             GildedRose app = new GildedRose(self_managing_items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.AreEqual(initial_quality_value, self_managing_items[0].Quality);
         }
 
@@ -33,10 +43,15 @@ namespace csharp
         [TestCase(-2)]
         public void The_quality_of_a_legendary_item_is_never_negative(int sellin)
         {
+            // Arrange
             int initial_quality_value = 0;
             IList<IItem> self_managing_items = new List<IItem> { new LegendaryItem(sellin, initial_quality_value) };
             GildedRose app = new GildedRose(self_managing_items);
+
+            // Act
             app.UpdateQuality();
+
+            // Assert
             Assert.IsTrue(self_managing_items[0].Quality >= 0);
         }
     }
