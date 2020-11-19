6e4b731a6cfc85eda41184887a7c464f6f880662
Clare Sudbery <clareworking@claresudbery.co.uk>
2020-11-19 19:10:31 +0000

    Refactor: Remove redundant code.

--- csharp/GildedRose.cs
+++ csharp/GildedRose.cs
@@ -14,32 +14,20 @@ namespace csharp
 
         public void UpdateQuality()
         {
-            for (var item_index = 0; item_index < _self_managing_items.Count; item_index++)
+            foreach (var item in _self_managing_items)
             {
-                Update_daily_quality_value(item_index);
-
-                Update_number_of_days_until_sell_by_date(item_index);
-
-                Update_quality_if_sell_by_date_has_passed(item_index);
+                item.Update_daily_quality_value();
+                item.Update_number_of_days_until_sell_by_date();
+                if (item.SellIn < 0)
+                {
+                    item.Update_quality_after_sell_by_date_has_passed();
+                }
             }
         }
 
-        private void Update_daily_quality_value(int item_index)
+        private IItem Item(int item_index)
         {
-            _self_managing_items[item_index].Update_daily_quality_value();
-        }
-
-        private void Update_number_of_days_until_sell_by_date(int item_index)
-        {
-            _self_managing_items[item_index].Update_number_of_days_until_sell_by_date();
-        }
-
-        private void Update_quality_if_sell_by_date_has_passed(int item_index)
-        {
-            if (_self_managing_items[item_index].SellIn < 0)
-            {
-                _self_managing_items[item_index].Update_quality_after_sell_by_date_has_passed();
-            }
+            return _self_managing_items[item_index];
         }
     }
 }
